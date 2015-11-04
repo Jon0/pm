677a8bfb5c4b653d2e6f27d4c4cb35e3e260d2a9
Jon0 <Jono4728@gmail.com>
2015-11-04 19:11:49 +1300

    script page

---
+++ source/shell/interface.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <os/pipe.h>
+#include <strings.h>
+
+#include "interface.h"
+
+std::string conf_path;
+std::string page_out;
+
+void set_conf_path(const char *confpath) {
+	conf_path = confpath;
+}
+
+const char *get_page(const char *page_name) {
+	os::exec(conf_path + "/source/shell/update.sh");
+	std::string page_path = conf_path + "/source/shell/shell.html";
+	page_out = io::read_file(page_path);
+	return page_out.c_str();
+}
